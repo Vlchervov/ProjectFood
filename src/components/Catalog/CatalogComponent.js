6230987b15449a69ab08b96e3ea9c4f5677097eb
Vladislav Chervov <Chervov017@gmail.com>
2023-02-13 17:02:27 +0700

    add Swiper

--- src/components/Catalog/CatalogComponent.js
+++ src/components/Catalog/CatalogComponent.js
@@ -1,8 +1,10 @@
 import { CategoryComponent } from "./CategoryComponent";
 import data from "../../data/categories.json";
+import { SwiperComponent } from "../Swiper/SwiperComponent";
 export const CatalogComponent = () => {
   return (
     <div className="catalog">
+      <SwiperComponent />
       {data.data.map((e) => (
         <CategoryComponent {...e} key={e.name} />
       ))}
---
+++ src/components/Swiper/SwiperComponent.js
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import SwiperCore, { Keyboard } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { useState } from "react";
+
+SwiperCore.use([Keyboard]);
+
+export const SwiperComponent = () => {
+  const [slider, setSlider] = useState(false);
+  const SwiperOptions = {
+    speed: 1000,
+    slidesPerView: 1,
+    loop: "enabled",
+    keyboard: {
+      onlyInViewport: true,
+    },
+    pagination: {
+      clickable: true,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+  };
+  return (
+    <Swiper
+      {...SwiperOptions}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      onSlideChange={() => setSlider(slider === false ? true : false)}
+    >
+      <SwiperSlide
+        onClick={() => {
+          console.log("Вы Тыкнули на котейку!");
+        }}
+      >
+        <img src="https://assets.htmlacademy.ru/img/blog/113/jpeg/jpeg1@1x.jpg"></img>
+      </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
